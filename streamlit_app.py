acc88ef8b45cf56df6fdb1b6268dc539d4879bca
puneetkhandujasnowflake <puneetkhandujasnowflake@gmail.com>
2026-03-30 08:22:06 +0530

    Update streamlit_app.py
    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,5 +53,5 @@ if Ingredients_list:
         success_message ='Your Smoothie is ordered,'+' '+name_on_order+'!'
         st.success(success_message, icon="✅")
 
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
