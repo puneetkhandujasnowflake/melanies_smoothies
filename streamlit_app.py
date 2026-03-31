010defa572e4c359b879e1592ad9ba9380b0a17f
puneetkhandujasnowflake <puneetkhandujasnowflake@gmail.com>
2026-03-31 07:05:09 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ if Ingredients_list:
     ingredients_string='' 
     for fruit_chosen in Ingredients_list:
         ingredients_string+=fruit_chosen + ' '
-        st.subheader(fruitchosen + 'Nutrition Information')
+        st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
         ##st.text(smoothiefroot_response.json())
         st_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
