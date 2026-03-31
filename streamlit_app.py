9e7541be25b2e3594e99448803e8ba8e8a713646
puneetkhandujasnowflake <puneetkhandujasnowflake@gmail.com>
2026-03-31 07:43:54 +0530

    Update streamlit_app.py
    pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages.
 import streamlit as st
 import requests
+import pandas as pd
 from snowflake.snowpark.functions import col
 
 # Write directly to the app.
@@ -25,6 +26,10 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 ##st.write(my_dataframe)
 ##st.dataframe(data=my_dataframe, use_container_width=True)
+##st.stop()
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 Ingredients_list = st.multiselect(
